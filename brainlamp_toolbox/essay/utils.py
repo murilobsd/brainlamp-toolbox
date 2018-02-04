cd27a89c933dd653e037417a1bc564cc01e1d5dc
Murilo Ijanc <murilo@lab804.com.br>
2018-02-04 16:55:25 -0200

    added get paragraphs with regex

--- brainlamp_toolbox/essay/utils.py
+++ brainlamp_toolbox/essay/utils.py
@@ -1,8 +1,11 @@
 """Utils essay."""
+import re
+
 import nltk
 
 sent_tokenizer = nltk.data.load('tokenizers/punkt/portuguese.pickle')
 stopwords = nltk.corpus.stopwords.words('portuguese')
+paragraphs_pattern = re.compile(r'\n\n+')
 
 
 def tokenize(text):
@@ -25,3 +28,14 @@ def get_sentences(text):
     rtype: list
     """
     return sent_tokenizer.tokenize(text)
+
+
+def get_paragraphs(text):
+    """Paragraph a string to split off new lines.
+
+    param text: tring to split
+    type text: str
+    return: list of string
+    rtype: list
+    """
+    return re.split(paragraphs_pattern, text)
